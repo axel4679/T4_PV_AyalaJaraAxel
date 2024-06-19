1627841180dc689ff5eab33286ad54e3cbd66023
axelayala <axelcapofg@gmail.com>
2024-06-19 00:03:13 -0300

    avance

--- src/main/java/ar/edu/unju/fi/model/Docente.java
+++ src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -1,59 +1,78 @@
 package ar.edu.unju.fi.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Docente {
-    private String legajo;
+	
+	private String legajo;
     private String nombre;
     private String apellido;
     private String email;
-    private String telefono;
+    private int telefono;
+    private String foto;
+	
+	public Docente() {
+		// TODO Auto-generated constructor stub
+	}
 
-    // Constructor, getters y setters
-    public Docente(String legajo, String nombre, String apellido, String email, String telefono) {
-        this.legajo = legajo;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.email = email;
-        this.telefono = telefono;
-    }
+	public Docente(String legajo, String nombre, String apellido, String email, int telefono, String foto) {
+		this.legajo = legajo;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+		this.telefono = telefono;
+		this.foto = foto;
+	}
 
-    // Getters y setters
-    public String getLegajo() {
-        return legajo;
-    }
+	public String getLegajo() {
+		return legajo;
+	}
 
-    public void setLegajo(String legajo) {
-        this.legajo = legajo;
-    }
+	public void setLegajo(String legajo) {
+		this.legajo = legajo;
+	}
 
-    public String getNombre() {
-        return nombre;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public String getApellido() {
-        return apellido;
-    }
+	public String getApellido() {
+		return apellido;
+	}
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getTelefono() {
-        return telefono;
-    }
+	public int getTelefono() {
+		return telefono;
+	}
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
+	
 }
--- src/main/java/ar/edu/unju/fi/model/Materia.java
+++ src/main/java/ar/edu/unju/fi/model/Materia.java
@@ -1,79 +1,117 @@
 package ar.edu.unju.fi.model;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Materia {
-    private String codigo;
-    private String nombre;
-    private String curso;
-    private int cantidadHoras;
-    private String modalidad; // "virtual" o "presencial"
+	private String codigo;
+	private String nombre;
+	private String curso;
+	private int cantidadHoras;
+	private Modalidad modalidad;
     private Docente docente;
     private Carrera carrera;
+    
+    public Materia() {
+		// TODO Auto-generated constructor stub
+	}
 
-    // Constructor, getters y setters
-    public Materia(String codigo, String nombre, String curso, int cantidadHoras, String modalidad, Docente docente, Carrera carrera) {
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.curso = curso;
-        this.cantidadHoras = cantidadHoras;
-        this.modalidad = modalidad;
-        this.docente = docente;
-        this.carrera = carrera;
-    }
+	
 
-    // Getters y setters
-    public String getCodigo() {
-        return codigo;
-    }
+	public Materia(String codigo, String nombre, String curso, int cantidadHoras, Modalidad modalidad, Docente docente,
+			Carrera carrera) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.curso = curso;
+		this.cantidadHoras = cantidadHoras;
+		this.modalidad = modalidad;
+		this.docente = docente;
+		this.carrera = carrera;
+	}
 
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
 
-    public String getNombre() {
-        return nombre;
-    }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+	public String getCodigo() {
+		return codigo;
+	}
 
-    public String getCurso() {
-        return curso;
-    }
 
-    public void setCurso(String curso) {
-        this.curso = curso;
-    }
 
-    public int getCantidadHoras() {
-        return cantidadHoras;
-    }
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
 
-    public void setCantidadHoras(int cantidadHoras) {
-        this.cantidadHoras = cantidadHoras;
-    }
 
-    public String getModalidad() {
-        return modalidad;
-    }
 
-    public void setModalidad(String modalidad) {
-        this.modalidad = modalidad;
-    }
+	public String getNombre() {
+		return nombre;
+	}
 
-    public Docente getDocente() {
-        return docente;
-    }
 
-    public void setDocente(Docente docente) {
-        this.docente = docente;
-    }
 
-    public Carrera getCarrera() {
-        return carrera;
-    }
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 
-    public void setCarrera(Carrera carrera) {
-        this.carrera = carrera;
-    }
+
+
+	public String getCurso() {
+		return curso;
+	}
+
+
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+
+
+	public int getCantidadHoras() {
+		return cantidadHoras;
+	}
+
+
+
+	public void setCantidadHoras(int cantidadHoras) {
+		this.cantidadHoras = cantidadHoras;
+	}
+
+
+
+	public Modalidad getModalidad() {
+		return modalidad;
+	}
+
+
+
+	public void setModalidad(Modalidad modalidad) {
+		this.modalidad = modalidad;
+	}
+
+
+
+	public Docente getDocente() {
+		return docente;
+	}
+
+
+
+	public void setDocente(Docente docente) {
+		this.docente = docente;
+	}
+
+
+
+	public Carrera getCarrera() {
+		return carrera;
+	}
+
+
+
+	public void setCarrera(Carrera carrera) {
+		this.carrera = carrera;
+	}
+
+	
 }
